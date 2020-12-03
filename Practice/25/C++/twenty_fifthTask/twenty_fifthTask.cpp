609c519b671625a052ebf55e890ed47707c60069
JustKelmory <polskiy_denis@mail.ru>
2020-12-03 14:50:33 +0300

    started 25 task

---
+++ Practice/25/C++/twenty_fifthTask/twenty_fifthTask.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int main()
+{
+	vector<int> array;
+	int n, m = 0;
+	int first;
+	int second;
+	bool pass;
+	cin >> n;
+	m = sqrt(n);
+	if (m % 2 == 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cin >> m;
+			array.push_back(m);
+		}
+	}
+	else if (m % 2 != 0)
+		return 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (array[i] < array[j])
+			{
+				i++;
+				j++;
+			}
+			else if (array[i] > array[j])
+				swap(i, j);
+		}
+	}
+	for (int i = 0; i < n; i++)
+		cout << array[i] << " " << endl;
+}
