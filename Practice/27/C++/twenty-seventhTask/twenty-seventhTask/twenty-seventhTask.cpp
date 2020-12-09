b38ff8f4a5329b6391fa00d796a4a9191f96e585
JustKelmory <polskiy_denis@mail.ru>
2020-12-09 20:21:28 +0300

    added 27 task

---
+++ Practice/27/C++/twenty-seventhTask/twenty-seventhTask/twenty-seventhTask.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n, a;
+    cin >> n;
+    vector<int> mass;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        mass.push_back(a);
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j < (n - i); j++) {
+            if (mass[j] % 10 > mass[j - 1] % 10) {
+                swap(mass[j - 1], mass[j]);
+            }
+            else if (mass[j] % 10 == mass[j - 1] % 10 and mass[j] > mass[j - 1]) {
+                swap(mass[j - 1], mass[j]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << mass[i] << " ";
+    }
+}
