d72afc9bf89a34c6120b866e60ac8405dea1caec
JustKelmory <71424662+JustKelmory@users.noreply.github.com>
2021-03-04 18:08:44 +0300

    adding files to tasks

---
+++ Practice/31/C++/task31/task31/task31.cpp
@@ -0,0 +1,36 @@
+﻿#include <vector>
+#include <fstream>
+
+std::ostream& operator<<(std::ostream& stream, std::vector<int>& vector)
+{
+	stream << vector.size() << "\t|";
+
+	for (auto& elem : vector)
+	{
+		stream << ' ' << &elem;
+	}
+
+	return stream;
+}
+
+int main()
+{
+	std::vector<int> vector;
+	std::ofstream file("data.txt");
+
+
+	for (int i = 0; i < 64; ++i)
+	{
+		vector.push_back(0);
+		file << vector << std::endl;
+	}
+
+	file << std::endl;
+
+	while (!vector.empty())
+	{
+		vector.pop_back();
+		file << vector << std::endl;
+	}
+
+	file.close();
